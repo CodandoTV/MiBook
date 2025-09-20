bb8746d4b55f5d7a445b7be1600b4dfed3f800ba
Gabriel Moro <gabrielbronzattimoro.es@gmail.com>
2025-09-20 12:52:51 -0300

    Unit tests for get_book_details

---
+++ test/layers/domain/fakes/fake_book_domain.dart
@@ -0,0 +1,10 @@
+import 'package:mibook/layers/domain/models/book_list_domain.dart';
+
+final fakeBookDomain = BookDomain(
+  id: 'id',
+  title: 'title',
+  description: 'description',
+  thumbnail: 'thumbnail',
+  authors: [],
+  kind: 'kind',
+);
---
+++ test/layers/domain/get_book_details_test.dart
@@ -0,0 +1,52 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mibook/layers/domain/repository/search_repository.dart';
+import 'package:mibook/layers/domain/usecases/get_book_details.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'fakes/fake_book_domain.dart';
+
+@GenerateNiceMocks([MockSpec<ISearchRepository>()])
+import 'get_book_details_test.mocks.dart';
+
+void main() {
+  late MockISearchRepository mockSearchRepository;
+  late GetBookDetails getBookDetails;
+
+  setUp(() {
+    mockSearchRepository = MockISearchRepository();
+    getBookDetails = GetBookDetails(
+      mockSearchRepository,
+    );
+  });
+
+  group('GetBookDetails', () {
+    test('searchById returns BookDetails on success', () async {
+      when(
+        mockSearchRepository.searchById(
+          id: 'id',
+        ),
+      ).thenAnswer((_) async => Future.value(fakeBookDomain));
+
+      final result = await getBookDetails.call(id: 'id');
+      expect(result, fakeBookDomain);
+    });
+
+    test('searchById returns BookDetails on failure', () async {
+      when(
+        mockSearchRepository.searchById(
+          id: 'id',
+        ),
+      ).thenThrow(Exception('Something went wrong'));
+
+      var error = false;
+      try {
+        await getBookDetails.call(id: 'id');
+        error = false;
+      } catch (e) {
+        error = true;
+      }
+
+      expect(error, isTrue);
+    });
+  });
+}
