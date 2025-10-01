d5cfbbfb9880f2148006949adf3007baed2b5215
Pedro Alvarez <14023675+pnalvarez@users.noreply.github.com>
2025-10-01 08:44:17 -0300

    [fix](pnalvarez): removed di.config.dart file and app module

--- lib/core/di/app_module.dart
+++
@@ -1,9 +0,0 @@
-import 'package:encrypted_shared_preferences/encrypted_shared_preferences.dart';
-import 'package:injectable/injectable.dart';
-
-@module
-abstract class AppModule {
-  @lazySingleton
-  EncryptedSharedPreferences get encryptedSharedPreferences =>
-      EncryptedSharedPreferences();
-}
