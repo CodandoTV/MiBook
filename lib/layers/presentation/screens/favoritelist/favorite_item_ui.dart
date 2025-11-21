688d315adc076555ac4033144fbfa1ab393f8e35
Pedro Alvarez <14023675+pnalvarez@users.noreply.github.com>
2025-11-21 14:22:19 -0300

    [fix](pnalvarez): FavoriteItemUI is freezed

--- lib/layers/presentation/screens/favoritelist/favorite_item_ui.dart
+++ lib/layers/presentation/screens/favoritelist/favorite_item_ui.dart
@@ -1,20 +1,17 @@
+import 'package:flutter/foundation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+part 'favorite_item_ui.freezed.dart';
 
 @freezed
-class FavoriteItemUI {
-  final String id;
-  final String kind;
-  final String title;
-  final String authors;
-  final String description;
-  final String? thumbnail;
+class FavoriteItemUI with _$FavoriteItemUI {
+  const FavoriteItemUI._();
 
-  FavoriteItemUI({
-    required this.id,
-    required this.kind,
-    required this.title,
-    required this.authors,
-    required this.description,
-    required this.thumbnail,
-  });
+  const factory FavoriteItemUI({
+    @Default('') id,
+    @Default('') kind,
+    @Default('') title,
+    @Default('') authors,
+    @Default('') description,
+    thumbnail,
+  }) = _FavoriteItemUI;
 }
