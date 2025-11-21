1681ad8b4687109a431a9104573b0f5a19a05d5c
Pedro Alvarez <14023675+pnalvarez@users.noreply.github.com>
2025-11-20 21:20:01 -0300

    [feat](pnalvarez): handling events on the viewmodel

--- lib/layers/presentation/screens/favoritelist/favorite_item_ui.dart
+++ lib/layers/presentation/screens/favoritelist/favorite_item_ui.dart
@@ -1,3 +1,5 @@
+import 'package:mibook/layers/domain/models/book_list_domain.dart';
+
 class FavoriteItemUI {
   final String id;
   final String kind;
@@ -14,4 +16,25 @@ class FavoriteItemUI {
     required this.description,
     required this.thumbnail,
   });
+
+  factory FavoriteItemUI.fromDomain(BookDomain domain) {
+    return FavoriteItemUI(
+      id: domain.id,
+      kind: domain.kind,
+      title: domain.title,
+      authors: (domain.authors).join(', '),
+      description: domain.description ?? '',
+      thumbnail: domain.thumbnail,
+    );
+  }
+
+  BookDomain get toDomain => BookDomain(
+    id: id,
+    kind: kind,
+    title: title,
+    authors: authors.isNotEmpty ? authors.split(', ') : [],
+    description: description,
+    thumbnail: thumbnail,
+    pageCount: 0,
+  );
 }
